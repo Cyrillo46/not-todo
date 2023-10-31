c9cbfb5802dd8fb9ad085661a56cc42e39000e88
Cyrillo46 <yahooisbetter46@gmail.com>
2023-10-31 18:10:45 -0400

    mapped over tasks and displayed them to list

--- not-todo/src/App.jsx
+++ not-todo/src/App.jsx
@@ -3,18 +3,47 @@ import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   const handleChange = (e) => {
     setValue(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTasks((currentTasks) => {
+      return [...currentTasks, { id: 0, title: value, inProgress: false }];
+    });
+
+    setValue("");
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor=""></label>
           <input value={value} onChange={handleChange} type="text" />
         </div>
         <button>Submit</button>
+      </form>
+      <form action="">
+        <div>
+          <h1>Here's what you shouldn't do...</h1>
+          <ul>
+            {tasks.map((task) => {
+              return (
+                <li key={task.id}>
+                  <label>
+                    <input type="radio" />
+                    {task.title}
+                  </label>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </form>
     </>
   );
