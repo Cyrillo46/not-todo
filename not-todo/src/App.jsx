9bd7ded2d1ba25e8d097440eef1d12ab72daecae
Cyrillo46 <yahooisbetter46@gmail.com>
2023-10-31 11:17:14 -0400

    set state for input

--- not-todo/src/App.jsx
+++ not-todo/src/App.jsx
@@ -1,11 +1,21 @@
-
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
     <>
-      
+      <form>
+        <div>
+          <label htmlFor=""></label>
+          <input value={value} onChange={handleChange} type="text" />
+        </div>
+        <button>Submit</button>
+      </form>
     </>
   );
 }
